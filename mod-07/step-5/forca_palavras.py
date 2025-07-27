5bac08d8359ec90a903f9ff6a745d009a835b537
Tiago Estêvão <Tiagoest6@gmail.com>
2025-07-27 01:45:32 -0300

    Módulo 7 completo inserido.

---
+++ mod-07/step-5/forca_palavras.py
@@ -0,0 +1,16 @@
+word_list = [
+    'abacate', 'abacaxi', 'abelha', 'abismo', 'água', 'agulha', 'alface', 'algodão', 'alicate', 'amarelo',
+    'amigo', 'amuleto', 'anel', 'antena', 'apanhar', 'apito', 'arroz', 'avião', 'azeite', 'azul',
+    'bala', 'banco', 'banana', 'barco', 'batata', 'bebê', 'bicho', 'bicicleta', 'bigode', 'bilhete',
+    'blusa', 'boi', 'bola', 'bolo', 'boneca', 'boneco', 'borboleta', 'braço', 'brinquedo', 'broto',
+    'cabelo', 'cabra', 'cachorro', 'cadeira', 'caderno', 'café', 'cama', 'camisa', 'caminho', 'caneta',
+    'canguru', 'canoa', 'carro', 'carta', 'casa', 'castelo', 'cavalo', 'celular', 'cimento', 'cinema',
+    'clarinete', 'cobra', 'colher', 'computador', 'copo', 'coração', 'corda', 'coruja', 'correr', 'cubo',
+    'dedo', 'dentista', 'desenho', 'diamante', 'dinheiro', 'dinossauro', 'dormir', 'dragão', 'duende', 'eco',
+    'elevador', 'elefante', 'embaixo', 'embaçado', 'energia', 'envelope', 'escada', 'escola', 'esfera', 'espelho',
+    'estrela', 'fada', 'falcão', 'ferramenta', 'festa', 'flauta', 'flor', 'fogão', 'foguete', 'folha',
+    'formiga', 'fósforo', 'frango', 'fruta', 'galinha', 'galo', 'garfo', 'gato', 'geladeira', 'gigante',
+    'giz', 'globo', 'goiaba', 'guitarra', 'hamburguer', 'helicóptero', 'herói', 'ilha', 'ímã', 'indio',
+    'instrumento', 'invadir', 'janela', 'jardim', 'jarra', 'javali', 'joaninha', 'jogador', 'jogo', 'joia',
+    'juiz', 'lâmpada', 'lapiseira', 'laranja', 'leão', 'legenda', 'lençol', 'letra', 'livro', 'lobo'
+]
